6cc5a18eb097a7622be18b7386bd98e2137db775
Javasaur <studtry@gmail.com>
2018-11-18 23:48:20 +0200

    Add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,13 @@ const tsProject = ts.createProject('tsconfig.json')
 gulp.task('transpile', () => {
     return tsProject.src()
         .pipe(sourcemaps.init())
-        .pipe(tsProject())
-        .js
+        .pipe(tsProject()).js
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'))
 })
 
 gulp.task('build', gulp.series('transpile'))
+
+gulp.task('watch', () => {
+    gulp.watch('src/**/*.ts').on('all', gulp.series('build'))
+})
